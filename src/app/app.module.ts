7e1d3ce5729f4f5988b2dc12a3457f2719be3d20
Evance23 <evancebarracks01@gmail.com>
2022-03-28 00:45:57 +0300

    Adding registration components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { ProfileComponent } from './profile/profile.component';
 import { BoardAdminComponent } from './board-admin/board-admin.component';
 import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
 import { BoardUserComponent } from './board-user/board-user.component';
-// import { authInterceptorProviders } from './_helpers/auth.interceptor';
+import { authInterceptorProviders } from './_helpers/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -29,7 +29,7 @@ import { BoardUserComponent } from './board-user/board-user.component';
     BrowserModule,
     AppRoutingModule
   ],
-  // providers: [authInterceptorProviders],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
