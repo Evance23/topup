080165610bda1f416858dfa4a6b2e6d8cc3c081a
Evance23 <evancebarracks01@gmail.com>
2022-03-28 07:25:09 +0300

    Add profile elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms'; 
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -22,9 +22,11 @@ import { authInterceptorProviders } from './_helpers/auth.interceptor';
     HomeComponent,
     ProfileComponent,
     BoardAdminComponent,
+    
     BoardModeratorComponent,
     BoardUserComponent
   ],
+  
   imports: [
     BrowserModule,
     AppRoutingModule
