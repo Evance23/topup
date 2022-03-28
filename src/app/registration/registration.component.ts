ab2aaa62b9fc5c5e62471ce750ab788e4dea9227
Evance23 <evancebarracks01@gmail.com>
2022-03-28 06:29:36 +0300

    Add registration validations

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  form: any ={
+    username : 'null',
+    email : 'null',
+    password : 'null'
+  }; 
+  isSuccessful = false;
+  isSignedUpFailed = false;
+  errorMessage = ''
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+  onSubmit(): void {
+    const { username, email, password } = this.form;
+    this.authService.register(username, email). subscribe(
+      // check out the addition of the password argument here
+      data=>{
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignedUpFailed = false;
+      },
+      err =>{
+        this.errorMessage = err.error.message;
+        this.isSignedUpFailed = true;
+      }
+    );
   }
 
 }
